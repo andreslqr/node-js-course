d2058c726473c170a5dbf7bf8d68c0cdc750ef37
andreslqr <me@andreslopez.com.mx>
2024-02-10 15:35:51 -0600

    basic form data received

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    
+    if(req.url == '/' && req.method == 'GET') {
+        res.setHeader('Content-Type', 'text/html')
+        res.write("<html>")
+        res.write("<head><form method='POST'><input type='text' name='name'><input type='submit'></head>")
+        res.write("<body><h1>hola</h1></body>")
+        res.write("</html>")
+        return res.end()
+    }
+
+    if(req.url == '/' && req.method == 'POST') {
+        const body = []
+        req.on('data', chunk => {
+            console.log(chunk)
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString().split('=')[1]
+            fs.writeFileSync('messages.txt', parsedBody)
+            res.end()
+        })
+    }
+
+
 })
 
 

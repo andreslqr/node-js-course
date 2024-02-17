7ad9ac299cb448fcb3336edb98fd756afa6d5b8a
andreslqr <me@andreslopez.com.mx>
2024-02-17 14:12:37 -0600

    added body parser

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,26 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/admin', (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.send('<h1>admin page</h1>')
 })
+
+router.get('/add-product', (req, res, next) => {
+    res.send(`
+        <form action="/admin/add-product" method="POST">
+            <input type="text" name="title" required>
+            <button type="submit">
+                Send
+            </button>
+        </form>
+    `)
+})
+
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/admin')
+})
+
+
 
 module.exports = router

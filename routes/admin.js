3d763fe490617ed357945e76f507b3821907292a
andreslqr <me@andreslopez.com.mx>
2024-02-17 20:54:23 -0600

    added pug template

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,23 @@
 const express = require('express')
-const { viewsPath } = require('./../helpers/path')
+const fs = require('fs')
+const { basePath, viewsPath } = require('./../helpers/path')
 
 const router = express.Router()
-
-router.get('/', (req, res, next) => {
-    res.sendFile(viewsPath('admin', 'index.html'))
-})
 
 router.get('/add-product', (req, res, next) => {
     res.sendFile(viewsPath('admin', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/admin')
+    const productsFile = basePath('products.json')
+    fs.readFile(productsFile, (err, data) => {
+        const products = JSON.parse(data ?? '[]')
+        products.push({
+            title: req.body.title
+        })
+        fs.writeFileSync(productsFile, JSON.stringify(products))
+    })
+    res.redirect('/')
 })
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,20 @@
 const express = require('express')
-const { viewsPath } = require('./../helpers/path')
+const fs = require('fs')
+const { basePath, viewsPath } = require('./../helpers/path')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.sendFile(viewsPath('shop', 'index.html'))
+    let products = [];
+    try {
+        products = JSON.parse(fs.readFileSync(basePath('products.json')))
+    } catch(err) {
+    }
+
+    res.render('shop', {
+        products,
+        metaTitle: "Shop"
+    })
 })
 
 module.exports = router
